010f6a0a6d7b38c5376a896a87d851429c5ada3f
Anton Dmitriev <tosha.dmitriefff@yandex.ru>
2024-02-21 20:50:36 +0100

    separate functions

--- main.c
+++ main.c
@@ -25,11 +25,11 @@
 #include <curses.h>         // Comfortable Input / Output with Terminal
 #include <windows.h>        // Windows API
 
-#define screenWidth 120                     // Console Screen Size X (Columns)
-#define screenHeight 30                     // Console Screen Size Y (Rows)
-#define mapWidth 16                         // World Size X (Columns)
-#define mapHeight 16                        // World Size Y (Rows)
-char map[mapHeight][mapWidth] = {           // Create Map of the World: '#' = wall / '.' = space    
+#define screenWidth 120             // Console Screen Size X (Columns)
+#define screenHeight 30             // Console Screen Size Y (Rows)
+#define mapWidth 16                 // World Size X (Columns)
+#define mapHeight 16                // World Size Y (Rows)
+char map[mapHeight][mapWidth] = {   // Create Map of the World: '#' = wall / '.' = space    
     {"################"},
     {"#..#...........#"},
     {"#..#....########"},
@@ -47,9 +47,10 @@ char map[mapHeight][mapWidth] = {           // Create Map of the World: '#' = wa
     {"#..............#"},
     {"################"}
 };
+float frameTime = 0.0f;             // Time of One Frame
 
-float playerX = 1.0f;			// Player Start Position
-float playerY = 1.00f;
+float playerX = 1.0f;			// Player Start Position X (Columns)
+float playerY = 1.00f;          // Player Start Position Y (Columns)
 float playerDir = 0.0f;         // Players Dirrection
 float playerSpeed = 5.0f;		// Walking Speed
 float playerFOV = M_PI / 3;     // Players Field of View
@@ -77,85 +78,101 @@ void setWindowSize()
     SetWindowLong(consoleWindow, GWL_STYLE, style);                     // Устанавливаем модифицированные стили обратно
 }
 
+void checkKeyState()
+{
+    if (GetAsyncKeyState((unsigned short)'A') & 0x8000)             // Обработка поворота влево
+    {
+        playerDir -= (playerSpeed * 0.75f) * frameTime;
+    }
+
+    if (GetAsyncKeyState((unsigned short)'D') & 0x8000)             // Обработка поворота вправо 
+    {
+        playerDir += (playerSpeed * 0.75f) * frameTime;
+    }
+
+    if (GetAsyncKeyState((unsigned short)'W') & 0x8000)             // Обработка движения вперед 
+    {
+	    playerX += sinf(playerDir) * playerSpeed * frameTime;
+	    playerY += cosf(playerDir) * playerSpeed * frameTime;
+
+	    if (map[(int)playerY][(int)playerX] == '#')                 // Обработка столкновения со стеной 
+	    {
+		    playerX -= sinf(playerDir) * playerSpeed * frameTime;
+		    playerY -= cosf(playerDir) * playerSpeed * frameTime;
+	    }			
+    }
+        
+    if (GetAsyncKeyState((unsigned short)'S') & 0x8000)             // Обработка движения назад 
+    {
+	    playerX -= sinf(playerDir) * playerSpeed * frameTime;
+	    playerY -= cosf(playerDir) * playerSpeed * frameTime;
+
+	    if (map[(int)playerY][(int)playerX] == '#')                 // Обработка столкновения со стеной 
+	    {
+		    playerX += sinf(playerDir) * playerSpeed * frameTime;
+		    playerY += cosf(playerDir) * playerSpeed * frameTime;
+	    }			
+    }
+
+    if (GetAsyncKeyState(VK_ESCAPE) & 0x8000)                       // Обработка выхода из программы
+    {
+        exit(1);
+    }
+}
+
+void displayStats()
+{
+    mvprintw(0,0,"x = %3.2f, y = %3.2f, direction = %3.2f, fps = %3.2f",playerX, playerY, playerDir, 1.0f / frameTime);
+
+    for (int i = 0; i < mapHeight; i++)
+    {
+        for (int j = 0; j < mapWidth; j++)
+        {
+            mvprintw(screenHeight - mapHeight + i,j,"%c",map[i][j]);
+        }
+    }
+}
+
+void renderFrame()
+{
+
+}
+
 int main() 
 {
-    clock_t timeBefore, timeAfter;  // Инициализируем временные пременные 
-    timeBefore = clock();           // Запоминаем текущее время
+    setWindowSize();                        // Initialization of Window Size
+
+    clock_t timeBefore, timeAfter;          // Инициализируем временные пременные 
+    timeBefore = clock();                   // Запоминаем текущее время 
     timeAfter = clock();
 
-    map[(int)playerY][(int)playerX] = 'P';      // Инициализация игрока на карте
+    map[(int)playerY][(int)playerX] = 'P';  // Инициализация игрока на карте
 
-    initscr();              // Инициализируем режим curses
-    cbreak();               // Отключаем буферизацию строк
-    noecho();               // Не отображаем вводимые символы
-    nodelay(stdscr, TRUE);  // Включаем неблокирующий режим для getch()
-    keypad(stdscr, TRUE);   // Получаем специальные клавиши, такие как стрелки
+    initscr();                              // Инициализируем режим curses
+    cbreak();                               // Отключаем буферизацию строк
+    noecho();                               // Не отображаем вводимые символы
+    nodelay(stdscr, TRUE);                  // Включаем неблокирующий режим для getch()
+    keypad(stdscr, TRUE);                   // Получаем специальные клавиши, такие как стрелки
 
-    while(1)
+    while(1)    // Game cycle
     {
-        timeAfter = clock();                                                    // Обновляем время конца кадра
-        float frameTime = (float)(timeAfter - timeBefore) / CLOCKS_PER_SEC;     // Подсчет времени кадра
-        timeBefore = timeAfter;                                                 // Обновляем время начала кадра
+        timeAfter = clock();                                            // Обновляем время конца кадра
+        frameTime = (float)(timeAfter - timeBefore) / CLOCKS_PER_SEC;   // Подсчет времени кадра
+        timeBefore = timeAfter;                                         // Обновляем время начала кадра
         
         map[(int)playerY][(int)playerX] = '.';  // Когда игрок начинает движение, он удаляется с карты
 
-        if (GetAsyncKeyState((unsigned short)'A') & 0x8000)             // Обработка поворота влево
-        {
-            playerDir -= (playerSpeed * 0.75f) * frameTime;
-        }
-
-        if (GetAsyncKeyState((unsigned short)'D') & 0x8000)             // Обработка поворота вправо 
-        {
-            playerDir += (playerSpeed * 0.75f) * frameTime;
-        }
-
-        if (GetAsyncKeyState((unsigned short)'W') & 0x8000)             // Обработка движения вперед 
-        {
-	        playerX += sinf(playerDir) * playerSpeed * frameTime;
-	        playerY += cosf(playerDir) * playerSpeed * frameTime;
-
-	        if (map[(int)playerY][(int)playerX] == '#')                 // Обработка столкновения со стеной 
-	        {
-		        playerX -= sinf(playerDir) * playerSpeed * frameTime;
-		        playerY -= cosf(playerDir) * playerSpeed * frameTime;
-	        }			
-        }
-        
-        if (GetAsyncKeyState((unsigned short)'S') & 0x8000)             // Обработка движения назад 
-        {
-	        playerX -= sinf(playerDir) * playerSpeed * frameTime;
-	        playerY -= cosf(playerDir) * playerSpeed * frameTime;
-
-	        if (map[(int)playerY][(int)playerX] == '#')                 // Обработка столкновения со стеной 
-	        {
-		        playerX += sinf(playerDir) * playerSpeed * frameTime;
-		        playerY += cosf(playerDir) * playerSpeed * frameTime;
-	        }			
-        }
-
-        if (GetAsyncKeyState(VK_ESCAPE) & 0x8000)                       // Обработка выхода из программы
-        {
-            return 1;
-        }
+        checkKeyState();                        // Check the Players Movement Depends on Key  
 
         map[(int)playerY][(int)playerX] = 'P';  // Когда игрок закончил движение, он добавляется на карту по новым координатам
 
-        // Вывод данных о игроке и миникарты
-        mvprintw(0,0,"x = %f, y = %f, direction = %f, fps = %f",playerX, playerY, playerDir, 1.0f / frameTime);
-
-        for (int i = 0; i < mapHeight; i++)
-        {
-            for (int j = 0; j < mapWidth; j++)
-            {
-                mvprintw(screenHeight - mapHeight + i,j,"%c",map[i][j]);
-            }
-        }
+        displayStats();                         // Display of Statistics and Minicart
     
-        refresh();      // Обновляем экран
+        refresh();                              // Обновляем кадр
 
     }
 
-    endwin();           // Выходим из curses
+    endwin();   // Выходим из curses
 
     return 0;
 }
